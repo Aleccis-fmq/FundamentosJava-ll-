0b7acc0fb5d3163ccdd58c3b46371095a023c291
Alexis <alexisxscfe@gmail.com>
2023-03-06 16:07:40 -0500

    Estructura de datos 163

---
+++ Proyecto157/src/arbol.java
@@ -0,0 +1,25 @@
+
+public class arbol {
+
+	//
+	class Nodo{
+		int info;
+		Nodo izq,der;
+	}
+	
+	
+	private Nodo raiz;
+	
+	private void insertar(int x) {
+		
+	}
+	
+	
+	
+	
+	public static void main (String [] args) {
+		//
+		arbol ar = new arbol();
+		
+	}
+}
