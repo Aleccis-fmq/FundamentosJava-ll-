e482224a6d0ae6a185bef1645aa8bf2b13c1a1c5
Alexis <alexisxscfe@gmail.com>
2023-05-19 10:07:39 -0500

    lous

---
+++ Proyecto186/src/MetodosEstaticos.java
@@ -0,0 +1,16 @@
+
+public class MetodosEstaticos {
+
+	public static void main (String [] args) {
+		
+		String cadena1 =  "255";
+		
+		int cad = Integer.parseInt(cadena1);
+		
+		System.out.println(cad);
+		
+		System.out.println(Integer.toBinaryString(cad));
+		
+		
+	}
+}
