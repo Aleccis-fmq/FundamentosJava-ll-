f69395a874327583d113b27c61e83afefb698cc5
Alexis <alexisxscfe@gmail.com>
2023-05-19 15:25:04 -0500

    nove

--- Proyecto186/src/MetodosEstaticos.java
+++ Proyecto186/src/MetodosEstaticos.java
@@ -9,8 +9,23 @@ public class MetodosEstaticos {
 		
 		System.out.println(cad);
 		
+		//binario
 		System.out.println(Integer.toBinaryString(cad));
+		//numero hexadecimal
+		System.out.println(Integer.toHexString(cad));
+		//numero octadecimal
+		System.out.println(Integer.toOctalString(cad));
 		
+		
+		
+		//cadena
+		//
+		String cad2 =String.valueOf(cad);
+		System.out.println(cad);
+		
+		float valor1 = 10.34442f;
+		int x2 = 300;
+		System.out.println(String.format("Valor de variable vcalor 1 %.1f / y x2 tiene %d", valor1 , x2));
 		
 	}
 }
